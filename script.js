fc0ac0345ebbe720ace0a9d972d7c11f84941d33
Saleh <zarasaleh.pro@gmail.com>
2025-07-12 21:35:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//      <div id="vanta-bg"></div>
+  VANTA.WAVES({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0x111111,
+    shininess: 50.00,
+    waveHeight: 20.00,
+    waveSpeed: 1.0,
+    zoom: 0.85
+  })
